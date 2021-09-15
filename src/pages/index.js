51a91f7b9a83bfe4aa914ca8b4f92ad4f7bd7369
Tin Tin <tint8140@gmail.com>
2021-09-15 15:24:11 -0400

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,15 @@
 import * as React from "react"
 
 const HomePage = () => {
-  return <h1>Home Page</h1>;
+  // return <h1>Home Page</h1>;
+  function onButtonClick() {
+    console.log('Hi there');
+}
+  (
+    <React.Fragment>
+        <h1>Home Page</h1>
+        <button>Click Me</button>
+    </React.Fragment>
+    );
 }
 export default HomePage;
